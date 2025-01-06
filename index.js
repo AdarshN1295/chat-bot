2d3cb4bd45c85d935ed7af4212896a3e7b1acef4
Adarsh Nigam <150742618+AdarshN1295@users.noreply.github.com>
2025-01-07 01:36:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
-const io = require('socket.io')(8000, {
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http, {
     cors: {
-        origin: "http://127.0.0.1:5500", // Use the exact origin where your client is hosted
+        origin: "*", // Allow any origin for testing; restrict in production
         methods: ["GET", "POST"]
     }
+});
+
+const PORT = process.env.PORT || 8000;
+
+// Serve static files from the root directory
+app.use(express.static(__dirname + '/../'));
+
+// Serve index.html at the root URL
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/../index.html');
 });
 
 const users = {};
 
 io.on('connection', (socket) => {
     socket.on('new-user-joined', (namee) => {
+        console.log('New User', namee);
         users[socket.id] = namee;
         socket.broadcast.emit('user-joined', namee);
     });
@@ -16,9 +30,8 @@ io.on('connection', (socket) => {
     socket.on('send', (message) => {
         socket.broadcast.emit('receive', { message: message, namee: users[socket.id] });
     });
+});
 
-    socket.on('disconnect', (message) => {
-        socket.broadcast.emit('left', users[socket.id]);
-        delete users[socket.id];
-    });
+http.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
